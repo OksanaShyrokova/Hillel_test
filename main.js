110f00dce3da69df227780c7e1f51241bd4472fd
Оксана <126983882+OksanaShyrokova@users.noreply.github.com>
2023-06-26 17:03:34 +0200

    mathematical operations

---
+++ main.js
@@ -0,0 +1,8 @@
+let x = Number (prompt('Enter number '));
+let y = Number(prompt('Enter number '));
+alert(`User entered ${x} and ${y}:\n
+${x} + ${y} = ${x+y}
+${x} - ${y} = ${x-y}
+${x} * ${y} = ${x*y}
+${x} / ${y} = ${x/y}
+`);
